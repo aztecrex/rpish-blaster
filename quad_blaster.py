a6a47f898bbff08b92e202e8cd2e6edd208e512e
Greg Wiley <aztec.rex@jammm.com>
2017-01-01 18:04:43 -0800

    plasma bolts

--- quad_blaster.py
+++ quad_blaster.py
@@ -2,9 +2,9 @@ from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 from signal import pause
 from line import plot_line
 from draw import Sprite
+from time import sleep
 
 sense = SenseHat()
-
 
 x = 3
 y = 3
@@ -40,28 +40,40 @@ def pushed_middle(event):
        
 
 def refresh():
-      draw()
+    draw()
 
-def pos(t, index):
-    return (0,0)
+def pos(progress, index):
+    cx,cy = corners[index]
+    return (cx + progress * (x - cx), cy + progress * (y - cy))
 
 def draw():
       global x, y, firing
       if not firing:
           crosshair.put((x, y))
       else:
-          for i in range(0, 10):
-              for j in range(0,4):
-                  bullets[j].put(pos(i,j))
+          firing = False
+          for i in range(0, 6):
+              progress = (1 / 6) * i
+              for j in [n for n in range(0,4) if corners[n] != (x,y)]:
+                  bolts[j].put(pos(progress,j))
+              crosshair.put((x, y))
+              sleep(.03)
+          bolts[0].put((x,y))
+          sleep(.1)
+          for bolt in bolts:
+              bolt.erase()
+          crosshair.put((x, y))
 
 sense.stick.direction_up = pushed_up
 sense.stick.direction_down = pushed_down
 sense.stick.direction_left = pushed_left
 sense.stick.direction_right = pushed_right
+sense.stick.direction_middle = pushed_middle
 sense.stick.direction_any = refresh
 
 crosshair = Sprite([(-1,0),(1,0),(0,-1),(0,1)], 0, 0, 255)
-bullets = [Sprite([(0,0)], 255, 0, 0) for i in range(0,4)]
+bolts = [Sprite([(0,0)], 255, 0, 0) for i in range(0,4)]
+corners = [(0,0),(0,7),(7,0),(7,7)]
 
 def main():
     sense.clear()
